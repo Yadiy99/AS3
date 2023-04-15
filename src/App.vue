9e1bd79b1e22bc716d00852ef3e891d65af72275
yadiy <21ftt1747@student.pb.edu.bn>
2023-04-15 08:53:52 +0800

    change background img

--- src/App.vue
+++ src/App.vue
@@ -17,15 +17,12 @@ import HelloWorld from './components/HelloWorld.vue'
   <div>
     <navbar-component></navbar-component>
     <main class="profile-page">
-      <section class="relative block" style="height: 500px;">
+      <section class="relative block" style="height: 500px; width: 700px;">
         <div
           class="absolute top-0 w-full h-full bg-center bg-cover"
-          style='background-image: url("https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80");'
+          style='background-image: url("background.jpg");'
         >
-          <!-- <span
-            id="blackOverlay"
-            class="w-full h-full absolute opacity-50 bg-black"
-          ></span> -->
+          
         </div>
         <div
           class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
@@ -52,7 +49,7 @@ import HelloWorld from './components/HelloWorld.vue'
           <div
             class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64"
           >
-            <div class="px-6">
+            <div class="px-6" style="background-color: antiquewhite;">
               <div class="flex flex-wrap justify-center">
                 <div
                   class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center"
@@ -62,7 +59,7 @@ import HelloWorld from './components/HelloWorld.vue'
                       alt="..."
                       src="yad.jpeg "
                       class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
-                      style="max-width: 100px; "
+                      style="max-width: 110px; "
                     />
                   </div>
                 </div>
@@ -95,11 +92,8 @@ import HelloWorld from './components/HelloWorld.vue'
                 <div class="flex flex-wrap justify-center">
                   <div class="w-full lg:w-9/12 px-4">
                     <p class="mb-4 text-lg leading-relaxed text-gray-800">
-                      An artist of considerable range, Jenna the name taken by
-                      Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                      performs and records all of his own music, giving it a
-                      warm, intimate feel with a solid groove structure. An
-                      artist of considerable range.
+                      I take it seriously when it comes to my club. I tried my best to teach them everything i know.
+
                     </p>
                     
                   </div>
