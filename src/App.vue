5c026bf7d2b10f7c17bdcc5c6b796711d3ae2274
Yadiy <98716467+Yadiy99@users.noreply.github.com>
2023-04-15 09:02:54 +0800

    change colour
    change background colour from white to beige

--- src/App.vue
+++ src/App.vue
@@ -52,7 +52,7 @@ import HelloWorld from './components/HelloWorld.vue'
           <div
             class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64"
           >
-            <div class="px-6">
+            <div class="px-6" style="background-color: antiquewhite;>
               <div class="flex flex-wrap justify-center">
                 <div
                   class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center"
