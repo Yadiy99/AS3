e75508912c254a32485a895a91d084f2f6a593f1
Yadiy <98716467+Yadiy99@users.noreply.github.com>
2023-04-15 08:51:42 +0800

    change background image
    change background image from website url to local file

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <section class="relative block" style="height: 500px;">
         <div
           class="absolute top-0 w-full h-full bg-center bg-cover"
-          style='background-image: url("https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80");'
+          style='background-image: url("background.jpg");'
         >
           <!-- <span
             id="blackOverlay"
